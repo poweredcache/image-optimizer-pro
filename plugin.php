fd3c2be1100e530676cb43f71015c5e5637d88f7
mustafauysal <me@uysalmustafa.com>
2023-12-18 11:25:24 +0100

    Update author name

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@
  * Version:           1.0
  * Requires at least: 5.7
  * Requires PHP:      7.2.5
- * Author:            PoweredCache
+ * Author:            Powered Cache
  * Author URI:        https://poweredcache.com/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
